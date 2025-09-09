06eed8e20f6411e635dfbc36471fe2b701ef0f0c
Chillibumbik <bm53557@gmail.com>
2025-09-09 14:38:02 +0300

    graphrag init

---
+++ libs/GDBInteracter.py
@@ -0,0 +1,19 @@
+from neo4j import GraphDatabase
+from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.generation import GraphRAG
+from neo4j_graphrag.embeddings import OpenAIEmbeddings
+
+class GDBInteracter:
+    def __init__(self):
+        """
+        Поднимает Графовую Базу Данных
+        """
+        pass
+    def create_graph(self, graph_name):
+        """
+
+        :param graph_name: id графа
+        :return:
+        """
+        pass
