f2c3c2d7be6e1a4d01ea6eec26e7a665a0bc85e8
Chillibumbik <bm53557@gmail.com>
2025-09-15 07:51:49 +0300

    added JSON responses with codes in /query

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def load(data: json):
     try:
         data = json.loads(data)
     except Exception as e:
-        return JSONResponse(content={'message': f'Не удалось сохранить файл\n{e}'}, status_code=400)
+        return JSONResponse(content={'message': f'Не удалось сохранить файл:\n{e}'}, status_code=400)
 
     try:
         """
@@ -40,53 +40,57 @@ def load(data: json):
         return JSONResponse(content={'message':'OK'}, status_code=200)
 
     except Exception as e:
-        return JSONResponse(content={'message': f'Bad Request\n{e}'}, status_code=500)
+        return JSONResponse(content={'message': f'Bad Request:\n{e}'}, status_code=500)
 
 
 # Получить ответ от сервиса по запросу
 @app.get("/query")
 def query(data: json):
-    data = json.loads(data)
-    """
-    data: {text: str, doc_id: str, dialogue: str}
-    """
+    try:
+        data = json.loads(data)
+        """
+        data: {text: str, doc_id: str, dialogue: str}
+        """
 
-    # перефразируем запрос на основе диалога
-    question = rewrite_question_from_dialogue(data["text"], data["dialogue"])
+        # перефразируем запрос на основе диалога
+        question = rewrite_question_from_dialogue(data["text"], data["dialogue"])
 
-    aspects = [] # список словарей, где каждый словарь - аспект с метаданными
+        aspects = [] # список словарей, где каждый словарь - аспект с метаданными
 
-    # выделяем аспекты из запроса
-    aspects_text = extract_aspects_from_question(question)
-    for aspect in aspects_text:
-        aspects.append({"text": aspect, "doc_id": data["doc_id"]})
+        # выделяем аспекты из запроса
+        aspects_text = extract_aspects_from_question(question)
+        for aspect in aspects_text:
+            aspects.append({"text": aspect, "doc_id": data["doc_id"]})
 
-    # векторизуем аспекты
-    for aspect in aspects:
-        aspect["dense_vector"] = embed(aspect["text"])
+        # векторизуем аспекты
+        for aspect in aspects:
+            aspect["dense_vector"] = embed(aspect["text"])
 
-    # [{text: str, dense_vector: list, doc_id: str}, {}, ...] - aspects сейчас
+        # [{text: str, dense_vector: list, doc_id: str}, {}, ...] - aspects сейчас
 
 
-    # dense поиск по qdrant
-    closest_chunks = []
-    for aspect in aspects:
-        closest_chunks.append(dense_search(aspect, topk=5))
+        # dense поиск по qdrant
+        closest_chunks = []
+        for aspect in aspects:
+            closest_chunks.append(dense_search(aspect, topk=5))
 
-    # получаем список списков, где каждый список - тексты ближайших чанков для каждого аспекта
+        # получаем список списков, где каждый список - тексты ближайших чанков для каждого аспекта
 
-    # поиск по neo4j. Получаем контекст
-    context = ""
-    for chunks in closest_chunks:
-        context += graph_context_from_chunks(chunks, data["doc_id"])
+        # поиск по neo4j. Получаем контекст
+        context = ""
+        for chunks in closest_chunks:
+            context += graph_context_from_chunks(chunks, data["doc_id"])
 
-    # по контексту делаем запрос в LLM
-    answer = answer_with_graph(question, context)
+        # по контексту делаем запрос в LLM
+        answer = answer_with_graph(question, context)
 
-    ### Еще можно сделать формирование id ответа и потом id ответа тоже передавать
+        ### Еще можно сделать формирование id ответа и потом id ответа тоже передавать
 
-    #оборачиваем в json и возвращаем
-    return json.dumps({"answer": answer, "doc_id": data["doc_id"]})
+        #оборачиваем в json и возвращаем
+        return JSONResponse(content={'answer': answer, 'doc_id': data["doc_id"]}, status_code=200)
+
+    except Exception as e:
+        return JSONResponse(content={'message': f'Internal Server Error:\n{e}'}, status_code=500)
 
 
 
