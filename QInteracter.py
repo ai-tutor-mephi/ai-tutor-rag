73c8cfa9c299324020888c0d893be0e20a5e75a3
Chillibumbik <bm53557@gmail.com>
2025-09-24 15:21:46 +0300

    added logs, async

--- QInteracter.py
+++ QInteracter.py
@@ -12,6 +12,13 @@ from Prompts import ASPECTS_SYS
 import os
 import dotenv
 
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    filename="Logs/qdrant.log",
+                    filemode="a",
+                    format="%(asctime)s [%(levelname)s] %(message)s")
+
 
 dotenv.load_dotenv()
 
@@ -22,12 +29,14 @@ class QInteracter:
     def __init__(self, client=client):
         self.client = client
     
-    def extract_aspects_from_question(self, question: str) -> list[str]:
+    async def extract_aspects_from_question(self, question: str) -> list[str]:
         """
         Extract aspects from the question
         :param question:
         :return:
         """
+
+        logging.info("Извлечение аспектов из вопроса...")
         client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url=os.getenv("OPENAI_API_BASE_URL"))
         resp = client.chat.completions.create(
             model=os.getenv("MS_LIGHT_MODEL"),
@@ -36,9 +45,10 @@ class QInteracter:
                 {"role": "user", "content": question}
             ]
         )
+        logging.info(f"Ответ модели для извлечения аспектов: {resp.choices[0].message.content}")
         return resp.choices[0].message.content.split("||")
     
-    def dense_search(self, query: dict, topk: int = 5) -> list[str]:
+    async def dense_search(self, query: dict, topk: int = 5) -> list[str]:
         """
         Dense search. Возвращает только текст topk ближайших чанков
         :param query: вектор запроса
@@ -53,6 +63,7 @@ class QInteracter:
         # )
 
         # запрос. Также тут поддерживается фукнция поиска по нескольким запросам сразу, если чуть поменять
+        logging.info("Выполнение dense поиска...")
         q = qm.Query(
             query_vector=("dense", query.get("dense_vector")),
             # query_filter=flt,
@@ -64,9 +75,11 @@ class QInteracter:
 
         resp = client.query_points(collection_name=query.get("doc_id"), query=[q])
         points = resp[0].points  # берем результаты первого (и единственного) запроса
+        
+        logging.info(f"Найдено {len(points)} ближайших чанков.")
         return [p.payload.get("text", "") for p in points]
     
-    def load_in_qdrant(self, chunks: list[dict]) -> None:
+    async def load_in_qdrant(self, chunks: list[dict]) -> None:
         """
         Функция создает коллекцию по id документа и загружает в нее данные. У каждого документа своя коллекция
         :param chunks: Чанки документа(вектора)
@@ -77,6 +90,7 @@ class QInteracter:
 
         # создать коллекцию
         # Для каждого документа своя коллекция
+        logging.info(f"Создание коллекции в Qdrant для документа {doc_id}...")
         client.create_collection(
             collection_name=doc_id,  # коллекцию называем по идентификатору документа, возможно, потом по другому
             vectors_config={
@@ -87,14 +101,19 @@ class QInteracter:
                 default_segment_number=2
             ),
         )
+        logging.info(f"Коллекция {doc_id} создана.")
 
+        logging.info(f"Создание индекса для метаданных в Qdrant для документа {doc_id}...")
         client.create_payload_index(
             collection_name=doc_id,
             field_name="doc_id",
             field_schema=PayloadSchemaType.KEYWORD
         )
 
+        logging.info(f"Индекс для метаданных в Qdrant для документа {doc_id} создан.")
 
+
+        logging.info(f"Загрузка данных в Qdrant для документа {doc_id}...")
         # загрузка данных
         points = []
         for ch in chunks:
@@ -125,5 +144,7 @@ class QInteracter:
         Если захотим гибридный поиск, что bgem3 надо будет локально развернуть, HF не поддерживает sparse
         """
 
+        logging.info(f"Загрузка {len(points)} точек в коллекцию {doc_id}...")
+        
         client.upsert(collection_name=doc_id, points=points)
 
